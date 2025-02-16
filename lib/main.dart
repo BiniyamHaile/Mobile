9d0918738d32b3de262dd74392327f3becdd3985
biniyamhaile <biniyhaile@gmail.com>
2025-02-16 08:58:23 +0300

    signup page added

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/ui/pages/login_page.dart';
+
+void main() => runApp(
+  const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+  )
+);
+
