ca2e6d6c17f5b1cbd6371096bcd55a8780fe8158
Daniel Tilahun <tiledan2015@gmail.com>
2025-06-09 01:50:43 +0300

    preference page

--- lib/ui/routes/app_routes.dart
+++ lib/ui/routes/app_routes.dart
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import 'package:go_router/go_router.dart';
-=======
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile/models/reel/privacy_option.dart';
 import 'package:mobile/models/reel/video_item.dart';
->>>>>>> 3caf1377c8e6acf894ba855e6244553e0b63682a
 import 'package:mobile/ui/pages/auth/forgot-password-page.dart';
 import 'package:mobile/ui/pages/auth/login_page.dart';
 import 'package:mobile/ui/pages/auth/otp_page.dart';
@@ -13,47 +9,10 @@ import 'package:mobile/ui/pages/auth/preference_page.dart';
 import 'package:mobile/ui/pages/auth/reset-password-page.dart';
 import 'package:mobile/ui/pages/auth/signup_page.dart';
 import 'package:mobile/ui/pages/chat_page.dart';
-<<<<<<< HEAD
-import 'package:mobile/ui/pages/post/feed_page.dart';
 import 'package:mobile/ui/pages/home_page.dart';
+import 'package:mobile/ui/pages/post/feed_page.dart';
 import 'package:mobile/ui/pages/post/post_page.dart';
 import 'package:mobile/ui/pages/profile/profile-setting-page.dart';
-import 'package:mobile/ui/pages/profile_page.dart';
-import 'package:mobile/ui/pages/story/user_story_page.dart';
-import 'package:mobile/ui/routes/route_names.dart';
-
-class AppRoutes {
-  static final router = GoRouter(
-    initialLocation: RouteNames.profileSetting,
-    routes: [
-    GoRoute(path: RouteNames.home, builder: (context, state) => const HomePage()
-    ),
-    GoRoute(path: RouteNames.feed, builder: (context, state) => const FeedPage()
-    ),
-    GoRoute(path: RouteNames.post, builder: (context, state) => PostingScreen()
-    ),
-    GoRoute(path: RouteNames.stories, builder: (context, state) {
-      final index = state.uri.queryParameters['index'];
-      return UserStoryPage(initialIndex:  int.parse(index ?? '0'));
-    }
-    ),
-    GoRoute(path: RouteNames.profile, builder: (context, state) => ProfilePage(),
-    ),
-    GoRoute(path: RouteNames.register, builder: (context, state) => const SignupPage()),
-    GoRoute(path: RouteNames.otp, builder: (context, state) => const OtpPage(),
-    ),
-    GoRoute(path: RouteNames.preferences, builder: (context, state) => const PreferencesPage()),
-    GoRoute(path: RouteNames.login, builder: (context, state) => const LoginPage()),
-    GoRoute(path: RouteNames.resetPassword, builder: (context, state) => const ResetPasswordPage()),
-    GoRoute(path: RouteNames.forgotPassword, builder: (context, state) => const ForgotPasswordPage()),
-    GoRoute(path: RouteNames.register, builder: (context, state) => const SignupPage()),
-    GoRoute(path: RouteNames.profileSetting, builder: (context, state) =>  SettingsPage()),
-    GoRoute(path: RouteNames.chat, builder: (context, state) => ChatPage())
-  ]);
-=======
-import 'package:mobile/ui/pages/home_page.dart';
-import 'package:mobile/ui/pages/post/feed_page.dart';
-import 'package:mobile/ui/pages/post/post_page.dart';
 import 'package:mobile/ui/pages/profile_page.dart';
 import 'package:mobile/ui/pages/story/user_story_page.dart';
 import 'package:mobile/ui/routes/route_names.dart';
@@ -69,7 +28,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AppRoutes {
   static final router = GoRouter(
-    initialLocation: RouteNames.home,
+    initialLocation: RouteNames.preferences,
     // Add the root-level redirect function here
     redirect: (context, state) async {
       // This redirect runs before ANY route is matched or navigated to.
@@ -130,6 +89,8 @@ class AppRoutes {
       return null; // Return null to allow navigation to the original path
     },
     routes: [
+      GoRoute(path: RouteNames.profileSetting, builder: (context, state) =>  SettingsPage()),
+      GoRoute(path: RouteNames.preferences, builder: (context, state) => const PreferencesPage()),
       GoRoute(
         path: RouteNames.home,
         builder: (context, state) => const HomePage(),
@@ -352,5 +313,4 @@ class AppRoutes {
       ),
     ],
   );
->>>>>>> 3caf1377c8e6acf894ba855e6244553e0b63682a
 }
